7e37074cf6ad008835fb6bf2d829bde189ba3fc2
Przemysław Ciżmiński <cizminski@interia.pl>
2026-06-26 19:29:42 +0200

    Added SQLRestriction to enable soft delete.

--- src/main/java/book/store/model/Book.java
+++ src/main/java/book/store/model/Book.java
@@ -9,10 +9,12 @@ import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import lombok.Data;
 import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 
 @Entity
 @Data
 @SQLDelete(sql = "UPDATE books SET is_deleted = true WHERE id = ?")
+@SQLRestriction("is_deleted = false")
 @Table(name = "books")
 public class Book {
 
