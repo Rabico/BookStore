23a01b5371b6b96a22f7033643279f8fb1ad1dcb
Przemysław Ciżmiński <cizminski@interia.pl>
2026-06-26 19:21:25 +0200

    Added Liquibase support. Implemented delete and update

--- src/main/java/book/store/model/Book.java
+++ src/main/java/book/store/model/Book.java
@@ -8,9 +8,11 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import lombok.Data;
+import org.hibernate.annotations.SQLDelete;
 
 @Entity
 @Data
+@SQLDelete(sql = "UPDATE books SET is_deleted = true WHERE id = ?")
 @Table(name = "books")
 public class Book {
 
@@ -27,4 +29,6 @@ public class Book {
     private BigDecimal price;
     private String description;
     private String coverImage;
+    @Column(nullable = false)
+    private boolean isDeleted = false;
 }
