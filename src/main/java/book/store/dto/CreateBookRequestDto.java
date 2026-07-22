d1984c31a790ccf93c4038f77868e1a3d32e7aec
Przemysław Ciżmiński <cizminski@interia.pl>
2026-07-22 21:17:22 +0200

    Create CustomGlobalExceptionHandler and prepared validetion for CreateBookRequestDto

--- src/main/java/book/store/dto/CreateBookRequestDto.java
+++ src/main/java/book/store/dto/CreateBookRequestDto.java
@@ -1,15 +1,30 @@
 package book.store.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import lombok.Data;
+import org.hibernate.validator.constraints.ISBN;
+import org.hibernate.validator.constraints.URL;
 
 @Data
 public class CreateBookRequestDto {
+    @NotNull
+    @Size(min = 1, max = 30)
     private String title;
+    @NotNull
+    @Size(min = 1, max = 30)
     private String author;
+    @NotNull
+    @ISBN
     private String isbn;
+    @NotNull
+    @Min(0)
     private BigDecimal price;
+    @Size(max = 100)
     private String description;
+    @URL(protocol = "https")
     private String coverImage;
 }
 
