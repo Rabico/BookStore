0639b59b138518eaf3ec685de7839be291e29846
Przemysław Ciżmiński <cizminski@interia.pl>
2026-07-23 19:25:01 +0200

    Apply mentor's feedback. Added custom messege to validation exceptions and added EntityNotFoundException to CustomGlobalExceptionHandler

--- src/main/java/book/store/dto/CreateBookRequestDto.java
+++ src/main/java/book/store/dto/CreateBookRequestDto.java
@@ -1,6 +1,7 @@
 package book.store.dto;
 
-import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
@@ -10,21 +11,21 @@ import org.hibernate.validator.constraints.URL;
 
 @Data
 public class CreateBookRequestDto {
-    @NotNull
-    @Size(min = 1, max = 30)
+    @NotBlank(message = "Tittle must not be blank")
+    @Size(max = 30, message = "Tittle must not exceed 30 characters")
     private String title;
-    @NotNull
-    @Size(min = 1, max = 30)
+    @NotBlank(message = "Tittle must not be blank")
+    @Size(max = 30, message = "Author must not exceed 30 characters")
     private String author;
-    @NotNull
-    @ISBN
+    @NotNull(message = "ISBN is required")
+    @ISBN(message = "ISBN must have ISBN pattern")
     private String isbn;
-    @NotNull
-    @Min(0)
+    @NotNull(message = "Price is required")
+    @DecimalMin(value = "0.0", message = "Price must be greater than zero")
     private BigDecimal price;
-    @Size(max = 100)
+    @Size(max = 100, message = "Author must not exceed 100 characters")
     private String description;
-    @URL(protocol = "https")
+    @URL(protocol = "https", message = "URL must by HTTPS protocol")
     private String coverImage;
 }
 
--- src/main/java/book/store/exception/CustomGlobalExceptionHandler.java
+++ src/main/java/book/store/exception/CustomGlobalExceptionHandler.java
@@ -13,6 +13,7 @@ import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -28,7 +29,7 @@ public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler
 
         Map<String, Object> body = new LinkedHashMap<>();
         body.put("timestamp", LocalDateTime.now());
-        body.put("status", HttpStatus.BAD_REQUEST);
+        body.put("status", HttpStatus.BAD_REQUEST.value());
         List<String> mappedErrors = ex.getBindingResult().getAllErrors().stream()
                 .map(this::getErrorMessage)
                 .toList();
@@ -41,8 +42,17 @@ public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler
         if (objectError instanceof FieldError fieldError) {
             String field = fieldError.getField();
             String defaultMessage = objectError.getDefaultMessage();
-            return field + " " + defaultMessage;
+            return field + ": " + defaultMessage;
         }
         return objectError.getDefaultMessage();
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Object> handleEntityNotFound(EntityNotFoundException ex) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", HttpStatus.NOT_FOUND.value());
+        body.put("message", ex.getMessage());
+        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
     }
 }
